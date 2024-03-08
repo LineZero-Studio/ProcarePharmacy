c39e5609d52908d60ec979342da54884905765f6
Taylor Williams <taylor.williams1999@outlook.com>
2024-03-08 16:24:16 -0500

    Made it so that the form can go between next and previous pages.

--- OnlineOrderForm/formControls.js
+++ OnlineOrderForm/formControls.js
@@ -3,7 +3,39 @@ let currentTab = 0;
 let locationElements = [];
 let locations = [];
 
-const selectionLocationDropdown = document.getElementById("selectionLocationDropdown");
+const formPages = Array.from(document.querySelectorAll('form .page'));
+const nextBtn = document.querySelectorAll('form .nextButton');
+const prevBtn = document.querySelectorAll('form .prevButton');
+const form = document.querySelector('form');
+
+// Event handlers for the next and previous buttons
+nextBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        changePage('next');
+    })
+});
+prevBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        changePage('previous');
+    })
+})
+
+// Change the form page after clicking a navigation button
+function changePage(btn) {
+    let index = 0;
+    const active = document.querySelector('form .page.active');
+    index = formPages.indexOf(active);
+    formPages[index].classList.remove('active');
+
+    if(btn === 'next') {
+        index++;
+    } else if(btn === 'previous') {
+        index--;
+    }
+
+    formPages[index].classList.add('active');
+    
+}
 
 function clickNextButton() {
     // validateFormInput
@@ -23,7 +55,7 @@ function retrieveLocationData() {
         var location = document.createElement("option");
         location.value = element.dataset.slug;
         location.text = element.dataset.locname;
-        selectionLocationDropdown.appendChild(location);
+        document.getElementById("selectionLocationDropdown").appendChild(location);
     });
 }
 
