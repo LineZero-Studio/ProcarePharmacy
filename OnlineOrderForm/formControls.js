088cb310575fafa340eb97b94403e27768d611a1
rainbowsludge <almaheart16@gmail.com>
2024-03-10 17:51:59 -0400

    Added all pages to form

--- OnlineOrderForm/formControls.js
+++ OnlineOrderForm/formControls.js
@@ -8,6 +8,9 @@ const nextBtn = document.querySelectorAll('form .nextButton');
 const prevBtn = document.querySelectorAll('form .prevButton');
 const form = document.querySelector('form');
 
+const addAnotherBtn = document.querySelectorAll('form .formInputListAddBtn');
+const cancelInputBtn = document.querySelectorAll('.formInputCancelButton')
+
 // Event handlers for the next and previous buttons
 nextBtn.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -19,6 +22,20 @@ prevBtn.forEach(button => {
         changePage('previous');
     })
 })
+
+addAnotherBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        addInputBox(e.target.closest(".page"));
+    })
+})
+
+cancelInputBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.target.parentElement.getElementsByClassName("formTextInputClearBtn")[0].value = "";
+    })
+})
+
+
 
 // Change the form page after clicking a navigation button
 function changePage(btn) {
@@ -37,7 +54,48 @@ function changePage(btn) {
     
 }
 
-// get locations data from the CMS and populate the form
+// Add an input and input cancel button to a list of inputs
+function addInputBox(currentPage) {
+    //const inputList = event.target.parentElement.getElementsByClassName("formInputListWithAddButton")[0];
+    const inputList = currentPage.getElementsByClassName("formInputListWithAddButton")[0];
+    
+    // Create empty elements for formInputCancelButtonPair, formTextInput, and formInputCancelButton
+    const newInputCancelBtnPair = document.createElement("div");
+    const newInput = document.createElement("input");
+    const newInputCancelBtn = document.createElement("img");
+
+    // Get the number of inputs in the list
+    const inputCount = Array.from(inputList.getElementsByClassName("formTextInput")).length;
+
+    // Create container for input and button
+    newInputCancelBtnPair.classList.add("formInputCancelButtonPair");
+
+    // Create new input to add to the list
+    newInput.id = inputList.id + "Entry" + (inputCount + 1);
+    newInput.name = inputList.id + "Entry" + (inputCount + 1);
+    newInput.classList.add("formTextInput");
+    newInput.classList.add("formTextInputClearBtn");
+    newInput.type = "text";
+    newInput.placeholder = "Enter RX number here";
+
+    // Create accompanying input cancel button
+    newInputCancelBtn.src = "input-cancel.svg";
+    newInputCancelBtn.classList.add("formInputCancelButton");
+    newInputCancelBtn.id = inputList.id + "CancelBtn" + (inputCount + 1);
+
+    // Add button event listener
+    newInputCancelBtn.addEventListener('click', (e) => { 
+        e.target.parentElement.remove();
+    });
+
+
+    // Add the elements to the list
+    inputList.appendChild(newInputCancelBtnPair);
+    newInputCancelBtnPair.appendChild(newInput);
+    newInputCancelBtnPair.appendChild(newInputCancelBtn);
+}
+
+// Get locations data from the CMS and populate the form
 function retrieveLocationData() {
     locationElements = document.querySelectorAll("div[id^='LOCATIONID_");
     locationElements.forEach((element) => {
