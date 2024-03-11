e58ff095934b4a1e3cc290af637975837418fbfb
rainbowsludge <almaheart16@gmail.com>
2024-03-10 21:28:22 -0400

    Changed syntax of observer target

--- OnlineOrderForm/formControls.js
+++ OnlineOrderForm/formControls.js
@@ -18,7 +18,7 @@ var observer = new MutationObserver(function (mutations) {
     }
 });
 
-observer.observe($(".order-online"), {
+observer.observe(document.querySelector(".order-online"), {
     childList: true,
     subtree: true
 });
