51bca166ec8c58b684a21b65e1bc25c9726ab49a
Giovanni Kane <it@linezerostudio.com>
2024-05-14 06:26:05 -0400

    Update formControls.js

--- OnlineOrderForm/formControls.js
+++ OnlineOrderForm/formControls.js
@@ -545,7 +545,9 @@ function initializeCustomSelect() {
       const urlParams = new URLSearchParams(window.location.search);
       const locationParam = urlParams.get("location"); // If location is passed in URL, set it as the default
 
-      if (locationParam === selElmnt.options[j].id.split("_")[1]) {
+      
+
+      if (locationParam && locationParam === document.querySelectorAll("div[id^='LOCATIONID_")[j].id.split("_")[1]) {
         c.click();
       }
 
